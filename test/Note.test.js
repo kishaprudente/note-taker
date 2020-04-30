a89b95782ea2d22a8e7a988939e2f4110a68e9da
Kisha Prudente <kisha.prudente@gmail.com>
2020-04-29 22:51:27 -0400

    created test for Note

---
+++ test/Note.test.js
@@ -0,0 +1,11 @@
+describe("Note", () => {
+  describe("Initialization", () => {
+    test("should create an object with a 'tite' property set to the 'tite' argument provided when called with the 'new' keyword", () => {
+      const title = "Do homework";
+      const text = "Unit 1";
+      const note = new Note(title, text);
+
+      expect(note.title).toEqual(title);
+    });
+  });
+});
