cd478d71de8fc12e303fec97cdfce38d5cb2cb11
Kisha Prudente <kisha.prudente@gmail.com>
2020-04-30 16:21:14 -0400

    imported routes to the server

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 const express = require("express");
 const path = require("path");
+const htmlRouter = require("./routes/htmlRoutes");
+const apiRouter = require("./routes/apiRoutes");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "notes.html"));
-});
-
-app.get("/api/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "db", "db.json"));
-});
+app.use("/", htmlRouter);
+app.use("/api", apiRouter);
 
 app.listen(PORT, () => {
   console.log("Server working! listening on: localhost:" + PORT);
